1a6b9fafe6e40699f91c5fef337e435094baf3a9
K dong min <user@MacBook-Pro.local>
2023-07-16 16:09:09 +0900

    Refactor [] output update

--- key_pair/key.tf
+++ key_pair/key.tf
@@ -2,3 +2,7 @@ resource "aws_key_pair" "key_pair" {
     key_name = var.key_name
     public_key = var.path_to_public_key
 }
+
+output "key_pair_id"{
+    value = aws_key_pair.key_pair.key_pair_id
+}
